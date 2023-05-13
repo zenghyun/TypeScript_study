64ac1a7189349a44f1512ce131debfa44210fff4
zenghyun <pipo36@naver.com>
2023-05-13 20:06:46 +0900

    Work Google_map

---
+++ 12_Google_map_practice/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devServer: {
+    devMiddleware: {
+      publicPath: '/dist/'
+    },
+    static: {
+      directory: path.resolve(__dirname)
+    }
+  },
+};
