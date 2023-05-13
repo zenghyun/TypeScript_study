559566bfd922cd5529022893e589b61948aad29a
zenghyun <pipo36@naver.com>
2023-05-13 20:38:57 +0900

    Work Google Maps

--- 12_Google_map_practice/src/app.ts
+++ 12_Google_map_practice/src/app.ts
@@ -1,41 +1,43 @@
 import axios from "axios";
 
 const form = document.querySelector("form")!;
-const addressInput = document.getElementById("address")! as HTMLInputElement;
+const address = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "AIzaSyCxcO08IzP5g6XpfgeNwSD9IuTlBK6lfS8";
+const GOOGLE_API_KEY = "발급받은 API KEY";
 
-declare var google: any;
+declare var google: any; 
 
 type GoogleGeocodingResponse = {
-    results: { geometry: { location: {lat: number, lng: number } } }[];
-    status: "OK" | "ZERO_RESULTS";
-}
-
-const searchAddressHandler = (event: Event) => {
-  event.preventDefault();
-  const enteredAddress = addressInput.value;
-
-  axios.get<GoogleGeocodingResponse>(
-    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
-      enteredAddress
-    )}&key=${GOOGLE_API_KEY}`
-  ).then(response => {
-    if (response.data.status !== "OK") {
-        throw new Error("Could not fetch location!");
-      }
-    const coordinates = response.data.results[0].geometry.location;
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: coordinates,
-        zoom: 16
-    });
-
-    new google.maps.Marker({position: coordinates, map: map});
-
-  }).catch(err => {
-    alert(err.message);
-    console.log(err);
-  });
+  results: { geometry: { location: { lat: number; lug: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
 };
 
-form.addEventListener("submit", searchAddressHandler);
+const submitHandler = (event: Event) => {
+  event.preventDefault();
+  const inputAddressValue = address.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        inputAddressValue
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not find address");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: coordinates,
+        zoom: 16
+      }); 
+
+      new google.maps.Marker({position: coordinates, map: map});
+    })
+    .catch((e) => {
+      alert(e.message);
+      console.log(e);
+    });
+};
+
+form.addEventListener("submit", submitHandler);
